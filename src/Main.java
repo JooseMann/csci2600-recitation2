8d0b41d3c564a97b030f49bc897c6e96b992a287
JooseMann <justinkarate76@gmail.com>
2026-09-11 18:25:51 -0400

    Updated Main.java to also use floats
    Commit #2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 public class Main {
 
     private static int[] nums = new int[5];
+    private static float[] floats = new float[5];
 
     public static void main(String[] args) {
         fillNums();
-
         System.out.println("Nums array:");
-
         printNums();
+
+        // Newline
+        System.out.println();
+
+        fillFloats();
+        System.out.println("Floats array:");
+        printFloats();
     }
 
     // Fills the nums array in reverse order (5, 4, 3, 2, 1)
@@ -17,10 +23,25 @@ public class Main {
         }
     }
 
+    // Fills the floats array uniformly (5.0f, 5.0f, 5.0f, 5.0f, 5.0f).
+    private static void fillFloats() {
+        for (int i = 0; i < floats.length; ++i) {
+            // Explicit cast not necessary, but helps with readability
+            floats[i] = (float) floats.length;
+        }
+    }
+
     // Prints the nums array.
     private static void printNums() {
         for (int i = 0; i < nums.length; ++i) {
             System.out.println(nums[i]);
+        }
+    }
+
+    // Prints the floats array.
+    private static void printFloats() {
+        for (int i = 0; i < floats.length; ++i) {
+            System.out.println(floats[i]);
         }
     }
 
